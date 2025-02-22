c183109b1da646b1fdeba9c17f1f6f9acd0ba9ac
rockydey <rockydey077@gmail.com>
2025-02-22 11:22:23 +0600

    explore basic mongoose

---
+++ src/app/modules/Student/student.controller.ts
@@ -0,0 +1,56 @@
+import { Request, Response } from 'express';
+import { StudentService } from './student.service';
+
+const createStudent = async (req: Request, res: Response) => {
+  try {
+    const { student: studentData } = req.body;
+
+    // Call service function to create student
+    const result = await StudentService.createStudentIntoDB(studentData);
+
+    // Send response
+    res.status(200).json({
+      success: true,
+      message: 'Student is created successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentService.getAllStudentsFromDB();
+
+    res.status(200).json({
+      success: true,
+      message: 'Students are retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+
+    const result = await StudentService.getSingleStudentFromDB(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved successfully',
+      data: result,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const StudentController = {
+  createStudent,
+  getAllStudents,
+  getSingleStudent,
+};
